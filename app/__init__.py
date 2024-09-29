fe92e193c980a6249c2252aa447a7baf8ef29f56
Fabiano Amaral Alves <fabianoamaral445@gmail.com>
2024-09-29 00:46:14 -0300

    deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,21 @@
-from app import app
+from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask_wtf import CSRFProtect
 
-if __name__ == '__main__':
-    app.run(debug = True)
+from app import views
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = 'a12qffsrwwq423313'
+    bootstrap = Bootstrap5(app)
+    csrf = CSRFProtect(app)
+    views.init_app(app)
+    return app
+
+app = create_app()
